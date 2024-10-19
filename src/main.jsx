0e8b9f31e2c0776357f48558d8d8acc390008bc5
Huỳnh Như Bảo Nhân <baonhannice@gmail.com>
2024-10-19 21:48:55 +0700

    dark mode - light mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import CssBaseline from '@mui/material/CssBaseline'
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
+import theme from './themes'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <CssVarsProvider theme = {theme}>
+      <CssBaseline />
+      <App />
+    </CssVarsProvider>
+  </React.StrictMode>
 )
