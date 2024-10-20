f6d2b8444047858973afe623bd5f4ebfa0f98511
Huỳnh Như Bảo Nhân <baonhannice@gmail.com>
2024-10-20 10:31:28 +0700

    refactor: folder structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
+import CssBaseline from '@mui/material/CssBaseline'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import CssBaseline from '@mui/material/CssBaseline'
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 import theme from './themes'
 
