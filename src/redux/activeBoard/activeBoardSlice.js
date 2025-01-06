f604daf5163b5cb1b1452c7287f9691a40552a02
Huỳnh Như Bảo Nhân <baonhannice@gmail.com>
2025-01-06 11:06:25 +0700

    config redux

---
+++ src/redux/activeBoard/activeBoardSlice.js
@@ -0,0 +1,73 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
+import { isEmpty } from 'lodash'
+import { API_URL } from '~/utils/constants'
+import { generatePlaceholder } from '~/utils/formatter'
+import { mapOrder } from '~/utils/sorts'
+
+const initialState = {
+  currentActiveBoard: null
+}
+
+export const fetchBoardDetailAPI = createAsyncThunk(
+  'activeBoard/fetchBoardDetailAPI',
+  async (boardId) => {
+    const response = await axios.get(`${API_URL}/boards/${boardId}`)
+    let board = response.data.data
+    board.lists = board.lists.map(list => ({ ...list, boardId: board.id }))
+    for (let list of board.lists) {
+      list.cards = list.cards.map(card => ({ ...card, listId: list.id }))
+    }
+    return board
+  }
+)
+
+export const activeBoardSlice = createSlice({
+  name: 'counter',
+  initialState,
+  //Reducer where we can define actions synchronously
+  reducers: {
+    updatecurrentActiveBoard: (state, action) => {
+      //standard puts the name data into the reducer, here we assign it to a more meaningful variable
+      const board = action.payload
+      board.lists = board.lists.map(list => ({ ...list, boardId: board.id }))
+      for (let list of board.lists) {
+        list.cards = list.cards.map(card => ({ ...card, listId: list.id }))
+      }
+      //update data currentActiveBoard
+      state.currentActiveBoard = board
+    }
+    //hanlde data needed for the board
+
+  },
+  //extraReducer for handling async actions
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchBoardDetailAPI.fulfilled, (state, action) => {
+        //action.payload is the data returned from the API
+        let board = action.payload
+        board.lists = mapOrder(board.lists, board.listOrderIds, 'id')
+        board.lists.forEach(list => {
+          if (isEmpty(list.cards)) {
+            list.cards = [generatePlaceholder(list)]
+            list.cardOrderIds = [generatePlaceholder(list).id]
+          } else
+            list.cards = mapOrder(list.cards, list.cardOrderIds, 'id')
+        })
+        //update data currentActiveBoard
+        state.currentActiveBoard = board
+
+      })
+  }
+
+})
+// action creator for the reducer, dispatch the action to the reducer
+export const { updatecurrentActiveBoard } = activeBoardSlice.actions
+// selector for the reducer, get data from the reducer
+export const selectcurrentActiveBoard = (state) => {
+  state.activeBoard.currentActiveBoard
+}
+
+
+// export default activeBoardSlice.reducer
+export const activeBoardReducer = activeBoardSlice.reducer
